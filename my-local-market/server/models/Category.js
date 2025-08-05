1ad97e8fb807a625869aa390f393ea5d42d49875
sohaibazmi101 <sohaibazmi101@gmail.com>
2025-08-05 20:38:18 +0530

    Initial Update

--- my-local-market/server/models/Category.js
+++ my-local-market/server/models/Category.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema({
   name: { type: String, required: true },
-  icon: { type: String }, // e.g., emoji or image URL
+  icon: { type: String },
 });
 
 module.exports = mongoose.model('Category', categorySchema);
