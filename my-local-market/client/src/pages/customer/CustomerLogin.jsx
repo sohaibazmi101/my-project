0af24e68e5266a4ea213934c7cb7fb771b2dc92f
sohaibazmi101 <sohaibazmi101@gmail.com>
2025-08-15 19:11:14 +0530

    Token removal from local storage

--- my-local-market/client/src/pages/customer/CustomerLogin.jsx
+++ my-local-market/client/src/pages/customer/CustomerLogin.jsx
@@ -26,6 +26,8 @@ export default function CustomerLogin() {
       setCustomer(res.data.customer);
       localStorage.removeItem('token');
       localStorage.removeItem('seller');
+      localStorage.removeItem('deliveryToken');
+      localStorage.removeItem('adminToken');
 
       alert('Logged in with Google!');
       navigate('/');
