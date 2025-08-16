0447aae791dffb3c91306c5bc04c1f4b016d8c74
sohaibazmi101 <sohaibazmi101@gmail.com>
2025-08-16 12:28:29 +0530

    Featured Count

--- my-local-market/server/controllers/productController.js
+++ my-local-market/server/controllers/productController.js
@@ -134,7 +134,7 @@ exports.toggleFeatured = async (req, res) => {
 
 exports.getFeaturedProducts = async (req, res) => {
   try {
-    const products = await Product.find({ featured: true }).limit(6);
+    const products = await Product.find({ featured: true }).limit(25);
     res.json(products);
   } catch (err) {
     res.status(500).json({ message: 'Server error' });
